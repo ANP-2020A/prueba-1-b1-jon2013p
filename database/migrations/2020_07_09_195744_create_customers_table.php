8f0d500cf00b2d5159a1be5af3bf11fb0d5b08ad
Jonathan Israel Vásquez Vivas <jon2013.p@hotmail.com>
2020-07-09 15:18:18 -0500

    #2 Modelos y migraciones

---
+++ database/migrations/2020_07_09_195744_create_customers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCustomersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name',255);
+            $table->string('lastname',255);
+            $table->string('document',88);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+}
